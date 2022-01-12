61716f1bdd1afd4926a1d333574dbd9d904f21ca
Piotr Usewicz <piotr@layer22.com>
2022-01-12 05:53:03 +0100

    Generate gem
    ```
    bundle gem statique --exe --coc --mit -t --ci --linter
    ```

---
+++ lib/statique.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "statique/version"
+
+module Statique
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "statique"
+
+require "minitest/autorun"
