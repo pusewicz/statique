8284cf9694d4f9637ea8631cdfa48fc4cc07ef5a
Piotr Usewicz <piotr@layer22.com>
2022-01-16 16:21:11 +0100

    Simplify starting and stopping the server

--- lib/statique/cli/server.rb
+++ lib/statique/cli/server.rb
@@ -2,19 +2,33 @@ require "puma"
 require "puma/configuration"
 require "puma/launcher"
 
+require_relative "../app"
+
 module Statique
   class CLI
     class Server
-      def initialize(options)
+      def initialize(port: 3000, background: true)
+        @port = port
+        @background = background
       end
 
       def run
-        config = Puma::Configuration.new({}) do |user_config, _|
-          user_config.app(App.freeze.app)
-          user_config.port(3000)
-        end
+        Statique.ui.info "Starting server on port #{@port}"
+        events = Puma::Events.new($stdout, $stderr)
+        @server = Puma::Server.new App.freeze.app, events
+        @server.add_tcp_listener("127.0.0.1", @port)
+        @server.run(@background)
 
-        Puma::Launcher.new(config).run
+        if @background
+          at_exit {
+            stop
+          }
+        end
+      end
+
+      def stop
+        Statique.ui.info "Stopping server on port #{@port}"
+        @server.stop(true)
       end
     end
   end
