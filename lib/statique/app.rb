990e25d915848492150a88fa84d8b9b319f192b2
Piotr Usewicz <piotr@layer22.com>
2022-01-20 00:16:43 +0100

    Group declarations together

--- lib/statique/app.rb
+++ lib/statique/app.rb
@@ -7,19 +7,29 @@ module Statique
   class App < Roda
     opts[:root] = Statique.pwd
 
-    if Statique.public?
-      plugin :public, root: Statique.public.basename
-    end
-
     plugin :environments
     plugin :static_routing
     plugin :render, views: Statique.content.basename, engine: "slim", allowed_paths: [Statique.content.basename, Statique.layouts.basename]
 
+    if Statique.mode.server? && Statique.public?
+      plugin :public, root: Statique.public.basename
+
+      route do |r|
+        r.public
+      end
+    end
+
     if Statique.assets?
       css_files = Statique.assets.join("css").glob("*.{css,scss}")
       js_files = Statique.assets.join("js").glob("*.js")
-      plugin :assets, css: css_files.map { _1.basename.to_s }, js: js_files.map { _1.basename.to_s }, public: Statique.destination
+      plugin :assets, css: css_files.map { _1.basename.to_s }, js: js_files.map { _1.basename.to_s }, public: Statique.destination, precompiled: Statique.destination.join("assets/manifest.json")
       plugin :assets_preloading
+
+      if Statique.mode.server?
+        route do |r|
+          r.assets
+        end
+      end
 
       Statique.mode.build do
         compiled = compile_assets
@@ -34,11 +44,6 @@ module Statique
         }
         view(document.view_name, engine: document.engine_name, layout: "../layouts/#{document.layout_name}", locals:)
       end
-    end
-
-    route do |r|
-      r.public if Statique.public?
-      r.assets if Statique.assets?
     end
   end
 end
