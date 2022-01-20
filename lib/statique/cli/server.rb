fd8a06258c9f409d9ac7df381f631619712ad83e
Piotr Usewicz <piotr@layer22.com>
2022-01-20 09:18:26 +0100

    Better logging

--- lib/statique/cli/server.rb
+++ lib/statique/cli/server.rb
@@ -3,25 +3,37 @@ require "rack"
 class Statique
   class CLI
     class Server
-      def initialize(port: 3000)
-        @port = port
-      end
-
       class LoggerWrapper
         def <<(msg)
-          method, uri, status = msg.chomp.split(":")
-          Statique.ui.info "#{method} #{uri}", status: status.to_i
+          method, uri, status, time, content_type = msg.chomp.split(":")
+          log(:info, "#{method} #{uri}", status: status.to_i, time: time.to_f, content_type:)
         end
+
+        def log(type, msg, attrs = {})
+          Statique.ui.public_send(type, msg, attrs.merge(app: "webrick"))
+        end
+
+        %i[unknown fatal error warn info debug].each do |type|
+          define_method(type) do |msg|
+            log(type, msg)
+          end
+        end
+      end
+
+      def initialize(port: 3000)
+        @port = port
       end
 
       def run
         Statique.ui.info "Starting server", port: @port
 
+        logger = LoggerWrapper.new
+
         Rack::Handler::WEBrick.run(Statique.instance.app,
           Port: @port,
           Host: "localhost",
-          Logger: Statique.ui,
-          AccessLog: [[LoggerWrapper.new, "%m:%U:%s"]])
+          Logger: logger,
+          AccessLog: [[logger, "%m:%U:%s:%T:%{Content-Type}o"]])
       end
 
       def stop
